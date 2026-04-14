19e34adfc3f381195736039a4b89657535a5de46
vishnugurjar123 <gurjarvishnu740@gmail.com>
2026-04-14 11:44:46 +0530

    d

--- server.js
+++ server.js
@@ -18,7 +18,10 @@ app.use(cors({
 }));
 // Mail transporter
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
