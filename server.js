fa5adfc7adb0606ea124922bad0471c20d844407
vishnugurjar123 <gurjarvishnu740@gmail.com>
2026-04-14 15:17:52 +0530

    f

--- server.js
+++ server.js
@@ -193,7 +193,7 @@ app.post("/send-email", async (req, res) => {
     // 📩 Admin notification
     const adminResponse = await resend.emails.send({
       from: "WellIndia <info@wellindia.in>",
-      to: ["management@wellindia.in"],
+      to: ["gurjarvishnu740@gmail.com"],
       subject: "🚀 New Service Request – Well India",
       html: adminEmailHTML({ name, email, mobile, services, location }),
     });
